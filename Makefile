23950218199408c80e0c7811b5d28054f6d4f0a6
Junichi TANAKA <junsan45@gmail.com>
2019-05-02 00:05:44 +0900

    Add cmd/server/main.go

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
 
+.PHONY: build
+build:
+	go build -o bin/server cmd/server/main.go
+
 pb/cat.pb.go: proto/cat.proto
 	protoc --go_out=plugins=grpc:pb -Iproto cat.proto
---
+++ cmd/server/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/junichi-tanaka/grpc/pb"
+	"github.com/junichi-tanaka/grpc/service"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	listenPort, err := net.Listen("tcp", ":19003")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	server := grpc.NewServer()
+	catService := &service.MyCatService{}
+	// 実行したい実処理をseverに登録する
+	pb.RegisterCatServer(server, catService)
+	server.Serve(listenPort)
+}

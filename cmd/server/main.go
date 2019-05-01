571ab5e3af023dc33dbc3b42131f0057bd82a616
Junichi TANAKA <junsan45@gmail.com>
2019-05-02 02:22:08 +0900

    Add zap logger

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,15 +7,28 @@ import (
 	pb "github.com/junichi-tanaka/grpc/pb"
 	"github.com/junichi-tanaka/grpc/service"
 
+	"github.com/grpc-ecosystem/go-grpc-middleware"
+	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
 )
 
 func main() {
+	zapLogger := zap.NewExample()
+	defer zapLogger.Sync()
+
 	listenPort, err := net.Listen("tcp", ":19003")
 	if err != nil {
 		log.Fatalln(err)
 	}
-	server := grpc.NewServer()
+	server := grpc.NewServer(
+		grpc.StreamInterceptor(grpc_middleware.ChainStreamServer(
+			grpc_zap.StreamServerInterceptor(zapLogger),
+		)),
+		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
+			grpc_zap.UnaryServerInterceptor(zapLogger),
+		)),
+	)
 	catService := &service.MyCatService{}
 	// 実行したい実処理をseverに登録する
 	pb.RegisterCatServer(server, catService)
--- go.mod
+++ go.mod
@@ -4,5 +4,9 @@ go 1.12
 
 require (
 	github.com/golang/protobuf v1.3.1
+	github.com/grpc-ecosystem/go-grpc-middleware v1.0.0
+	go.uber.org/atomic v1.3.2 // indirect
+	go.uber.org/multierr v1.1.0 // indirect
+	go.uber.org/zap v1.10.0
 	google.golang.org/grpc v1.20.1
 )
